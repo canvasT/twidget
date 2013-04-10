63225a42010b847fa08380bae6dc93b25f923f3a
canvast <fengtaotao082913@gmail.com>
2013-04-10 15:12:17 +0800

    删除多余的继承机制

--- src/base.js
+++ src/base.js
@@ -113,12 +113,16 @@
 
 	P('Record').Class = JY.Class;
 
-	// 对Date的扩展，将 Date 转化为指定格式的String 
-	// 月(M)、日(d)、小时(h)、分(m)、秒(s)、季度(q) 可以用 1-2 个占位符， 
-	// 年(y)可以用 1-4 个占位符，毫秒(S)只能用 1 个占位符(是 1-3 位的数字) 
-	// 例子： 
-	// (new Date()).Format("yyyy-MM-dd hh:mm:ss.S") ==> 2006-07-02 08:09:04.423 
-	// (new Date()).Format("yyyy-M-d h:m:s.S")      ==> 2006-7-2 8:9:4.18 
+	
+	/**
+	 * 对Date的扩展，将 Date 转化为指定格式的String 
+	 * 月(M)、日(d)、小时(h)、分(m)、秒(s)、季度(q) 可以用 1-2 个占位符， 
+	 * 年(y)可以用 1-4 个占位符，毫秒(S)只能用 1 个占位符(是 1-3 位的数字) 
+	 * 例子： 
+	 * (new Date()).Format("yyyy-MM-dd hh:mm:ss.S") ==> 2006-07-02 08:09:04.423 
+	 * (new Date()).Format("yyyy-M-d h:m:s.S")      ==> 2006-7-2 8:9:4.18 
+	 * @param {String} fmt 格式字符串
+	 */
 	Date.prototype.Format = function(fmt){ //author: meizz 
 		var o = { 
 		    "M+" : this.getMonth()+1,                 //月份 
@@ -152,24 +156,6 @@
 			var argc = Array.prototype.slice.call(args, 0);
 			Array.prototype.push.apply(argc, arguments);
 			return fn.apply(that || window, argc);
-		};
-	};
-
-	P('Util').objectCopy = function(o){
-		var F = function(){};
-		F.prototype = o;
-		return new F();
-	};
-
-	P('Util').extend = function(Sub, Super, ext){
-		var key;
-		var proto = Util.objectCopy(Super.prototype);
-		proto.constructor = Sub;
-		Sub.super = Super.prototype;
-		Sub.prototype = proto;
-
-		for(key in ext){
-			Sub.prototype[key] = ext[key];
 		};
 	};
 })()
