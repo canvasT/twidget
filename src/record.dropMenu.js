4770d5c02de007246b81c00b0c3fa2614f42756c
canvast <fengtaotao082913@gmail.com>
2013-04-10 14:31:37 +0800

    实现addOption、removeOption和updateModel接口

--- src/record.dropMenu.js
+++ src/record.dropMenu.js
@@ -13,7 +13,7 @@ $(function(){
 	            classPrefix: 'ui-dropmenu',
 	            name: null,
 	            //value: null,
-	            length: 0,
+	            //length: 0,
 	            selectedIndex: 0,
 	            disabled: false
 	        };
@@ -23,23 +23,40 @@ $(function(){
 	        this._container = (this._opts.container ? $(this._opts.container) : $('<div><div>')).appendTo('body');
 	        this._widgetId = wUtil.getId('widget');
 	        this._positioned = false;
+	        this._model = this._opts.model || [];
 	        this.name = this._opts.name;
-	        this.value = this._opts.name;
+	        //this.value = this._opts.name;
+	        this.length = this._model.length;
 
 	        this._container.addClass(this._opts.classPrefix).attr('widget-id', this._widgetId).hide();
 	       	this._trigger.addClass(this._opts.classPrefix + '-trigger');
 	    },
+	    /**
+	     * 生成组件所需的dom节点
+	     * @return null
+	     */
 	    _initDom: function(){
+	    	this._updateList();
+	    },
+	    /**
+	     * 更新下拉框dom
+	     * @return null
+	     */
+	    _updateList: function(){
 	    	var that = this;
 	    	var content = ['<ul class="', this._opts.classPrefix, '-content" data-role="content">'];
-	    	$.each(this._opts.model, function(i, o){
+	    	$.each(this._model, function(i, o){
 	    		content.push('<li class="', that._opts.classPrefix, '-item" data-role="item" data-value="', o.value, 
 	    			'" data-selected="', (o.selected ? true : false), '">', o.text, '</li>');
 	    	})
 	    	content.push('</ul>');
 
-	    	this._container.html(content.join(''));
+	    	this._container.html(content.join(''));	
 	    },
+	    /**
+	     * 绑定组件所需的事件
+	     * @return null
+	     */
 	    _bindEvent: function(){
 	    	var that = this;
 	    	this._trigger.bind('click', function(e){
@@ -52,19 +69,37 @@ $(function(){
 		    			that._container.css({
 		    				top: tOffset.top + that._trigger.outerHeight(),
 		    				left: tOffset.left,
-		    				position: 'absolute'
+		    				position: 'absolute',
+		    				'z-index': '99'
 		    			});
 		    		}
 		    		that._container.show();
 	    		}
 	    	})
 
+	    	this._bindListEvent();
+	    },
+	    /**
+	     * 绑定下拉菜单相关的事件
+	     * 在更新下拉列表时特别有用
+	     * @return null
+	     */
+	    _bindListEvent: function(){
+	    	var that = this;
 	    	this._container.find('[data-role=item]').bind('click', function(e){
 	    		e.stopPropagation();
 	    		that.select($(this));
-	    		
 	    	})
 	    },
+	    /**
+	     * 选中某一项
+	     * @param  {Number|String|DOM} item 
+	     *         						支持三种：
+		 * 								列表索引，为 Number
+		 * 								选择器，为 String
+		 * 								DOM，支持 DOM Element 和 jQuery 对象
+	     * @return null
+	     */
 	    select: function(item){
 	    	var $seltItem
 	    	if(typeof item == 'number'){
@@ -77,23 +112,59 @@ $(function(){
 	    	if($seltItem.length > 0){
 	    		this.value = $seltItem.attr('data-value');
 	    		this._trigger.text($seltItem.text());
+		    	this.trigger('change', this.value, $seltItem.text());
 	    	}
 
 	    	this._container.hide();
-	    	this.trigger('change');
 	    },
-	    updateModel: function(){
-
+	    /**
+	     * 更新组件数据，并重新生成列表
+	     * @param  {Object} model 格式：
+	     *                        	[
+		 *							    {value:'value1', text: 'text1', selected: true},
+		 *							    {value:'value2', text: 'text2'}
+		 *							]
+	     * @return null
+	     */
+	    updateModel: function(model){
+	    	if(!!model){
+	    		this._model = model
+	    	}
+	    	this.select(0);
+	    	this.length = this._model.length;
+	    	this._updateList();
+	    	this._bindListEvent();
 	    },
-	    addOption: function(){
-
+	    /**
+	     * 将一个选项添加到最后
+	     * @param {Object} option 选项数据：{value: 'value1', text: 'text1'}
+	     */
+	    addOption: function(option){
+	    	this._model.push(option);
+	    	this.updateModel();
 	    },
 	    getOption: function(){
-
+	    	//TODO
 	    },
-	    removeOption: function(){
+	    /**
+	     * 删除某个选项
+	     * @param  {Number} option 选项的索引值，从0开始
+	     * @return null
+	     */
+	    removeOption: function(option){
+	    	if(this.length <= 1)
+	    		return false;
 
+	    	if(typeof option == 'number'){
+	    		var index = Math.min(option, this.length - 1);
+	    		this._model.splice(index, 1);
+	    		this.updateModel();
+	    	}
 	    },
+	    /**
+	     * 组件渲染函数，与init分开的目的是不需要在组件初始化时就生成节点或绑定事件，再有需要的时候再进行渲染
+	     * @return {Object} 组件实例对象，参考jQuery的链式调用
+	     */
 	    render: function(){
 	    	this._initDom();
 	    	this._bindEvent();
--- src/record.event.js
+++ src/record.event.js
@@ -1,15 +1,37 @@
+/**
+ * Event类
+ * 继承自该类的组件将具有事件驱动功能，包括事件绑定与事件触犯等
+ */
 P('Record').Event = Record.Class({
 	init: function(){
 		this._eventMap = {};
 	},
+	/**
+	 * 绑定事件
+	 * @param  {String} type   事件类型
+	 * @param  {Function} handle 处理函数
+	 * @return null
+	 */
 	bind: function(type, handle){
 		var eventList = this._eventMap[type] = this._eventMap[type] ? this._eventMap[type] : [];
 
 		eventList.push(handle);
 	},
+	/**
+	 * 解绑
+	 * 删除指定事件类型下的处理函数
+	 * @param  {String} type 事件类型
+	 * @return null
+	 */
 	unbind: function(type){
 		this._eventMap[type] = null;
 	},
+	/**
+	 * 触发事件
+	 * 执行指定事件类型下的处理函数
+	 * @param  {String} type 事件类型
+	 * @return null
+	 */
 	trigger: function(type){
 		var eventList = this._eventMap[type];
 		var args = Array.prototype.slice.apply(arguments, [1])
